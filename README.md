ee004bf2ea092dac3b202af932429097a3b8ed90
Flux Xu <fluxxu@gmail.com>
2021-03-11 17:45:33 -0500

    repo clean up & test improvement

--- README.md
+++ README.md
@@ -1,35 +1,49 @@
 # StructMapper
+
 A library to help you generate code that mapped one struct to another.
 
 # Example
+
 ```rust
-  use structmapper::StructMapper;
-  struct From1 {
+use structmapper::StructMapper;
+
+struct From1 {
+    base: i32,
     value: i32,
-  }
-  struct From2 {
+}
+
+struct From2 {
     value: i32,
-  }
-  struct From3 {
+}
+
+struct From3 {
     value: i32,
-  }
-  #[derive(StructMapper)]
-  #[struct_mapper(from_type = "From1")]
-  #[struct_mapper(from_type = "From2")]
-  #[struct_mapper(
+}
+
+#[derive(StructMapper)]
+#[struct_mapper(from_type = "From1", fields(base = "Default::default()"))]
+#[struct_mapper(from_type = "From2", fields(base = "Default::default()"))]
+#[struct_mapper(
     from_type = "(From1, From2, From3)",
     default_base = "{0}",
-    fields(
-      value = "{0.value} + {1.value} + {2.value}"
-    )
-  )]
-  struct To {
-    value: i32
-  }
-  let to = To::from(From1 { value: 1 });
-  assert_eq!(to.value, 1);
-  let to = To::from(From2 { value: 2 });
-  assert_eq!(to.value, 2);
-  let to = To::from((From1 { value: 1 }, From2 { value: 2 }, From3 { value: 3 }));
-  assert_eq!(to.value, 1 + 2 + 3);
+    fields(value = "{0.value} + {1.value} + {2.value}")
+)]
+struct To {
+    base: i32,
+    value: i32,
+}
+
+let to = To::from(From1 { base: 0, value: 1 });
+assert_eq!(to.value, 1);
+
+let to = To::from(From2 { value: 2 });
+assert_eq!(to.value, 2);
+
+let to = To::from((
+    From1 { base: 42, value: 1 },
+    From2 { value: 2 },
+    From3 { value: 3 },
+));
+assert_eq!(to.base, 42);
+assert_eq!(to.value, 1 + 2 + 3);
 ```
--- crates/structmapper-test/src/lib.rs
+++ crates/structmapper-test/src/lib.rs
@@ -1,66 +1,66 @@
 #[test]
 fn test_derive() {
-  use structmapper::StructMapper;
+    use structmapper::StructMapper;
 
-  struct From1 {
-    value: i32,
-  }
+    struct From1 {
+        base: i32,
+        value: i32,
+    }
 
-  struct From2 {
-    value: i32,
-  }
+    struct From2 {
+        value: i32,
+    }
 
-  struct From3 {
-    value: i32,
-  }
+    struct From3 {
+        value: i32,
+    }
 
-  #[derive(StructMapper)]
-  #[struct_mapper(from_type = "From1")]
-  #[struct_mapper(from_type = "From2")]
-  #[struct_mapper(
-    from_type = "(From1, From2, From3)",
-    default_base = "{0}",
-    fields(
-      value = "{0.value} + {1.value} + {2.value}"
-    )
-  )]
-  struct To {
-    value: i32
-  }
+    #[derive(StructMapper)]
+    #[struct_mapper(from_type = "From1", fields(base = "Default::default()"))]
+    #[struct_mapper(from_type = "From2", fields(base = "Default::default()"))]
+    #[struct_mapper(
+        from_type = "(From1, From2, From3)",
+        default_base = "{0}",
+        fields(value = "{0.value} + {1.value} + {2.value}")
+    )]
+    struct To {
+        base: i32,
+        value: i32,
+    }
 
-  let to = To::from(From1 { value: 1 });
-  assert_eq!(to.value, 1);
+    let to = To::from(From1 { base: 0, value: 1 });
+    assert_eq!(to.value, 1);
 
-  let to = To::from(From2 { value: 2 });
-  assert_eq!(to.value, 2);
+    let to = To::from(From2 { value: 2 });
+    assert_eq!(to.value, 2);
 
-  let to = To::from((From1 { value: 1 }, From2 { value: 2 }, From3 { value: 3 }));
-  assert_eq!(to.value, 1 + 2 + 3);
+    let to = To::from((
+        From1 { base: 42, value: 1 },
+        From2 { value: 2 },
+        From3 { value: 3 },
+    ));
+    assert_eq!(to.base, 42);
+    assert_eq!(to.value, 1 + 2 + 3);
 }
 
 #[test]
 fn test_field_override() {
-  use structmapper::StructMapper;
+    use structmapper::StructMapper;
 
-  #[allow(unused)]
-  struct From {
-    a: i32,
-    b: i32,
-  }
+    #[allow(unused)]
+    struct From {
+        a: i32,
+        b: i32,
+    }
 
-  #[derive(StructMapper)]
-  #[struct_mapper(
-    from_type = "From",
-    fields(
-      a = "1234"
-    )
-  )]
-  struct To {
-    a: i32,
-    b: i32,
-  }
+    #[derive(StructMapper)]
+    #[struct_mapper(from_type = "From", fields(a = "1234"))]
+    struct To {
+        a: i32,
+        b: i32,
+    }
 
-  let to = To::from(From { a: 1, b: 2 });
-  assert_eq!(to.a, 1234);
-  assert_eq!(to.b, 2);
+    let to = To::from(From { a: 1, b: 2 });
+    assert_eq!(to.a, 1234);
+    assert_eq!(to.b, 2);
 }
